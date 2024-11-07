09b5d77be36aafd458ae3a74781c674601c03d13
vyvy31082004 <phanthaovy31082004@gmail.com>
2024-11-07 13:13:59 +0700

    Vy

--- smartPrint/src/App.jsx
+++ smartPrint/src/App.jsx
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App()  {
   return (
     
--- smartPrint/src/Pages/FileUploader.jsx
+++ smartPrint/src/Pages/FileUploader.jsx
@@ -7,66 +7,63 @@ export default function FileUploader ()  {
     return (
             <div className="d-flex flex-column p-4">
                 <div className="d-flex justify-content-between align-items-center mb-2">
-                    <h1>TẢI TỆP</h1>
+                    <h1>CHỌN TỆP</h1>
                     <h4>Phan Thảo Vy</h4>
                 </div>
                 <section className="" style={{backgroundColor: "white", padding: '24px'}}>
-                    <div className="d-flex justify-content-evenly mb-2"> 
+                    <div className="d-flex justify-content-between mb-2"> 
                         <h5>Danh sách tệp đã tải lên</h5>
-                        <i class="bi bi-file-earmark-arrow-up-fill"></i>
+                        <div className='btn btn-secondary'>
+                            <i class="bi bi-cloud-arrow-up pe-2"></i>
+                            Tải tệp
+                        </div>
+                        
                         <div className='btn btn-secondary'>Gần nhất</div>
+                       
+                        
                     </div>
                     <hr />
+                    
                     <div className="d-flex justify-content-between">
-                    <table className="table">
+                        <table className="table">
                             <thead>
                                 <tr>
-                                <th scope="col">ID Máy In</th>
-                                <th scope="col">Tên máy</th>
-                                <th scope="col">Thời gian</th>
-                                <th scope="col">Người dùng</th>
                                 <th scope="col">Tên tệp</th>
                                 <th scope="col">Số trang</th>
                                 <th scope="col">Kích thước</th>
-                                <th scope="col">Loại tệp</th>
+                                <th scope="col">Loại tệp </th>
+                                
+                        
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                <td>P001</td>
-                                <td>Máy in A</td>
-                                <td>2024-10-10 13:24:17</td>
-                                <td>Phan Thao Vy</td>
                                 <td>BTVN</td>
                                 <td>10</td>
                                 <td>A4</td>
                                 <td>PDF</td>
                                 </tr>
                                 <tr>
-                                <td>P002</td>
-                                <td>Máy in B</td>
-                                <td>2024-09-19 15:24:17</td>
-                                <td>Phan Thao Vy</td>
-                                <td>BTVN2</td>
-                                <td>1</td>
-                                <td>A4</td>
-                                <td>PDF</td>
-                                </tr>
-                                <tr>
-                                <td>P003</td>
-                                <td>Máy in c</td>
-                                <td>2024-08-30 07:30:56</td>
-                                <td>Phan Thao Vy</td>
                                 <td>CNPM</td>
                                 <td>2</td>
                                 <td>A4</td>
                                 <td>PDF</td>
                                 </tr>
+                                <tr>
+                                <td>HDC</td>
+                                <td>1</td>
+                                <td>A4</td>
+                                <td>PDF</td>
+                                </tr>
                             </tbody>
                     </table>
+                    
                     </div>
                 </section>
-
+                <div className="d-flex justify-content-end mb-2"> 
+                    <div className='btn btn-secondary'>Tiếp theo</div>
+                    <div className='btn btn-secondary1'>Trở về</div>
+                </div>
             </div>
         
 
