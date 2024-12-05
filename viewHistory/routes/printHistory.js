dd0c626764026327bf18303f48b0e969dd4b49a9
KietoKun <phuonggiakiet2004@gmail.com>
2024-12-05 14:48:26 +0700

    update printhistory

---
+++ viewHistory/routes/printHistory.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const PrintHistoryController = require('../controller/PrintHistoryController');
+
+// Lấy lịch sử in
+router.get('/print-history', PrintHistoryController.getPrintHistory);
+
+// Thêm lịch sử in mới
+router.post('/add', PrintHistoryController.addPrintHistory);
+
+module.exports = router;
